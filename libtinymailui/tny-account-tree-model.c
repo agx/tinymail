f564e54c7dd9c777fc20f926a587bc2da1443308
Philip Van Hoof <philip@codeminded.be>
2006-02-07 23:23:59 +0000

    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@17 d4f517bd-a70b-0410-9f68-c17655437c52

--- libtinymailui/tny-account-tree-model.c
+++ libtinymailui/tny-account-tree-model.c
@@ -20,15 +20,15 @@
 #include <glib.h>
 #include <gtk/gtk.h>
 
-#include <tny-msg-account-tree-model.h>
-#include <tny-msg-account-iface.h>
+#include <tny-account-tree-model.h>
+#include <tny-account-iface.h>
 #include <tny-msg-folder-iface.h>
 
 static GObjectClass *parent_class = NULL;
 
 
 static void
-fill_treemodel_recursive (TnyMsgAccountTreeModel *self, const GList *folders, GtkTreeIter *parent_iter)
+fill_treemodel_recursive (TnyAccountTreeModel *self, const GList *folders, GtkTreeIter *parent_iter)
 {
 	GtkTreeStore *model = GTK_TREE_STORE (self);
 
@@ -41,10 +41,10 @@ fill_treemodel_recursive (TnyMsgAccountTreeModel *self, const GList *folders, Gt
 		gtk_tree_store_append (model, &iter, parent_iter);
 
 		gtk_tree_store_set (model, &iter,
-			TNY_MSG_ACCOUNT_TREE_MODEL_NAME_COLUMN, 
+			TNY_ACCOUNT_TREE_MODEL_NAME_COLUMN, 
 			tny_msg_folder_iface_get_name (folder),
-			TNY_MSG_ACCOUNT_TREE_MODEL_UNREAD_COLUMN, 0,
-			TNY_MSG_ACCOUNT_TREE_MODEL_INSTANCE_COLUMN,
+			TNY_ACCOUNT_TREE_MODEL_UNREAD_COLUMN, 0,
+			TNY_ACCOUNT_TREE_MODEL_INSTANCE_COLUMN,
 			folder, -1);
 
 		if (more_folders && g_list_length ((GList*)more_folders) > 0)
@@ -56,60 +56,60 @@ fill_treemodel_recursive (TnyMsgAccountTreeModel *self, const GList *folders, Gt
 }
 
 void
-tny_msg_account_tree_model_add (TnyMsgAccountTreeModel *self, TnyMsgAccountIface *account)
+tny_account_tree_model_add (TnyAccountTreeModel *self, TnyAccountIface *account)
 {
 	GtkTreeStore *model = GTK_TREE_STORE (self);
-	const GList *folders = tny_msg_account_iface_get_folders (account);
+	const GList *folders = tny_account_iface_get_folders (account);
 
 	fill_treemodel_recursive (self, folders, NULL);
 
 	return;
 }
 
-TnyMsgAccountTreeModel*
-tny_msg_account_tree_model_new (void)
+TnyAccountTreeModel*
+tny_account_tree_model_new (void)
 {
-	TnyMsgAccountTreeModel *self = g_object_new (TNY_MSG_ACCOUNT_TREE_MODEL_TYPE, NULL);
+	TnyAccountTreeModel *self = g_object_new (TNY_ACCOUNT_TREE_MODEL_TYPE, NULL);
 
 	return self;
 }
 
 static void
-tny_msg_account_tree_model_finalize (GObject *object)
+tny_account_tree_model_finalize (GObject *object)
 {
 	(*parent_class->finalize) (object);
 }
 
 static void
-tny_msg_account_tree_model_class_init (TnyMsgAccountTreeModelClass *class)
+tny_account_tree_model_class_init (TnyAccountTreeModelClass *class)
 {
 	GObjectClass *object_class;
 
 	parent_class = g_type_class_peek_parent (class);
 	object_class = (GObjectClass*) class;
 
-	object_class->finalize = tny_msg_account_tree_model_finalize;
+	object_class->finalize = tny_account_tree_model_finalize;
 
-	/* g_type_class_add_private (object_class, sizeof (TnyMsgAccountTreeModelPriv)); */
+	/* g_type_class_add_private (object_class, sizeof (TnyAccountTreeModelPriv)); */
 
 	return;
 }
 
 static void
-tny_msg_account_tree_model_instance_init (GTypeInstance *instance, gpointer g_class)
+tny_account_tree_model_instance_init (GTypeInstance *instance, gpointer g_class)
 {
 	GtkTreeStore *store = (GtkTreeStore*) instance;
 	static GType types[] = { G_TYPE_STRING, G_TYPE_INT, G_TYPE_POINTER };
 
 	gtk_tree_store_set_column_types (store, 
-		TNY_MSG_ACCOUNT_TREE_MODEL_N_COLUMNS, types);
+		TNY_ACCOUNT_TREE_MODEL_N_COLUMNS, types);
 
 	return;
 }
 
 
 GType
-tny_msg_account_tree_model_get_type (void)
+tny_account_tree_model_get_type (void)
 {
 	static GType type = 0;
 
@@ -117,18 +117,18 @@ tny_msg_account_tree_model_get_type (void)
 	{
 		static const GTypeInfo info = 
 		{
-		  sizeof (TnyMsgAccountTreeModelClass),
+		  sizeof (TnyAccountTreeModelClass),
 		  NULL,   /* base_init */
 		  NULL,   /* base_finalize */
-		  (GClassInitFunc) tny_msg_account_tree_model_class_init,   /* class_init */
+		  (GClassInitFunc) tny_account_tree_model_class_init,   /* class_init */
 		  NULL,   /* class_finalize */
 		  NULL,   /* class_data */
-		  sizeof (TnyMsgAccountTreeModel),
+		  sizeof (TnyAccountTreeModel),
 		  0,      /* n_preallocs */
-		  tny_msg_account_tree_model_instance_init    /* instance_init */
+		  tny_account_tree_model_instance_init    /* instance_init */
 		};
 
-		type = g_type_register_static (GTK_TYPE_TREE_STORE, "TnyMsgAccountTreeModel",
+		type = g_type_register_static (GTK_TYPE_TREE_STORE, "TnyAccountTreeModel",
 					    &info, 0);
 	}
 
---
+++ libtinymailui/tny-account-tree-model.h
@@ -0,0 +1,65 @@
+#ifndef TNY_ACCOUNT_TREE_MODEL_H
+#define TNY_ACCOUNT_TREE_MODEL_H
+
+/* libtinymailui - The Tiny Mail UI library
+ * Copyright (C) 2006-2007 Philip Van Hoof <pvanhoof@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with self library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <glib.h>
+#include <gtk/gtk.h>
+#include <tny-account-iface.h>
+
+G_BEGIN_DECLS
+
+#define TNY_ACCOUNT_TREE_MODEL_TYPE            (tny_account_tree_model_get_type ())
+#define TNY_ACCOUNT_TREE_MODEL(obj)            (GTK_CHECK_CAST ((obj), TNY_ACCOUNT_TREE_MODEL_TYPE, TnyAccountTreeModel))
+#define TNY_ACCOUNT_TREE_MODEL_CLASS(klass)    (GTK_CHECK_CLASS_CAST ((klass), TNY_ACCOUNT_TREE_MODEL_TYPE, TnyAccountTreeModelClass))
+#define TNY_ACCOUNT_IS_TREE_MODEL(obj)         (GTK_CHECK_TYPE ((obj), TNY_ACCOUNT_TREE_MODEL_TYPE))
+#define TNY_ACCOUNT_IS_TREE_MODEL_CLASS(klass) (GTK_CHECK_CLASS_TYPE ((obj), TNY_ACCOUNT_TREE_MODEL_TYPE))
+#define TNY_ACCOUNT_TREE_MODEL_GET_CLASS(obj)  (GTK_CHECK_GET_CLASS ((obj), TNY_ACCOUNT_TREE_MODEL_TYPE, TnyAccountTreeModelClass))
+
+typedef struct _TnyAccountTreeModel TnyAccountTreeModel;
+typedef struct _TnyAccountTreeModelClass TnyAccountTreeModelClass;
+
+enum 
+{
+	TNY_ACCOUNT_TREE_MODEL_NAME_COLUMN,
+	TNY_ACCOUNT_TREE_MODEL_UNREAD_COLUMN,
+	TNY_ACCOUNT_TREE_MODEL_INSTANCE_COLUMN,
+	TNY_ACCOUNT_TREE_MODEL_N_COLUMNS
+};
+
+struct _TnyAccountTreeModel
+{
+	GtkTreeStore parent;
+};
+
+struct _TnyAccountTreeModelClass
+{
+	GtkTreeStoreClass parent_class;
+};
+
+
+GType                   tny_account_tree_model_get_type  (void);
+TnyAccountTreeModel* tny_account_tree_model_new       (void);
+
+void                    tny_account_tree_model_add       (TnyAccountTreeModel *self, TnyAccountIface *account);
+
+G_END_DECLS
+
+#endif
