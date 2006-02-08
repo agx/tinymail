bbb0d9e1983ade50e336878f7e4a5d02a2e3eb81
Philip Van Hoof <philip@codeminded.be>
2006-02-08 00:13:20 +0000

    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@20 d4f517bd-a70b-0410-9f68-c17655437c52

---
+++ tinymail/tny-account-factory.c
@@ -0,0 +1,108 @@
+/* libtinymail - The Tiny Mail base library
+ * Copyright (C) 2006-2007 Philip Van Hoof <pvanhoof@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with self library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <glib.h>
+
+#include <tny-account-factory.h>
+
+static GObjectClass *parent_class = NULL;
+
+
+typedef struct _TnyAccountFactoryPriv TnyAccountFactoryPriv;
+
+struct _TnyAccountFactoryPriv
+{
+};
+
+#define TNY_ACCOUNT_FACTORY_GET_PRIVATE(o)	\
+	(G_TYPE_INSTANCE_GET_PRIVATE ((o), TNY_ACCOUNT_FACTORY_TYPE, TnyAccountFactoryPriv))
+
+
+
+TnyAccountFactory*
+tny_account_factory_new (void)
+{
+	TnyAccountFactory *self = g_object_new (TNY_ACCOUNT_FACTORY_TYPE, NULL);
+
+	return self;
+}
+
+static void
+tny_account_factory_instance_init (GTypeInstance *instance, gpointer g_class)
+{
+	TnyAccountFactory *self = (TnyAccountFactory *)instance;
+	TnyAccountFactoryPriv *priv = TNY_ACCOUNT_FACTORY_GET_PRIVATE (self);
+
+	return;
+}
+
+static void
+tny_account_factory_finalize (GObject *object)
+{
+	TnyAccountFactory *self = (TnyAccountFactory *)object;	
+	TnyAccountFactoryPriv *priv = TNY_ACCOUNT_FACTORY_GET_PRIVATE (self);
+
+	(*parent_class->finalize) (object);
+
+	return;
+}
+
+static void 
+tny_account_factory_class_init (TnyAccountFactoryClass *class)
+{
+	GObjectClass *object_class;
+
+	parent_class = g_type_class_peek_parent (class);
+	object_class = (GObjectClass*) class;
+
+	object_class->finalize = tny_account_factory_finalize;
+
+	g_type_class_add_private (object_class, sizeof (TnyAccountFactoryPriv));
+
+	return;
+}
+
+GType 
+tny_account_factory_get_type (void)
+{
+	static GType type = 0;
+
+	if (type == 0) 
+	{
+		static const GTypeInfo info = 
+		{
+		  sizeof (TnyAccountFactoryClass),
+		  NULL,   /* base_init */
+		  NULL,   /* base_finalize */
+		  (GClassInitFunc) tny_account_factory_class_init,   /* class_init */
+		  NULL,   /* class_finalize */
+		  NULL,   /* class_data */
+		  sizeof (TnyAccountFactory),
+		  0,      /* n_preallocs */
+		  tny_account_factory_instance_init    /* instance_init */
+		};
+
+		type = g_type_register_static (G_TYPE_OBJECT,
+			"TnyAccountFactory",
+			&info, 0);
+	}
+
+	return type;
+}
+
---
+++ tinymail/tny-account-factory.h
@@ -0,0 +1,54 @@
+#ifndef TNY_ACCOUNT_FACTORY_H
+#define TNY_ACCOUNT_FACTORY_H
+
+/*
+ * Copyright (C) 2006-2007 Philip Van Hoof <pvanhoof@gnome.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with self program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <glib.h>
+#include <glib-object.h>
+#include <tny-shared.h>
+
+G_BEGIN_DECLS
+
+#define TNY_ACCOUNT_FACTORY_TYPE             (tny_account_get_type ())
+#define TNY_ACCOUNT_FACTORY(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), TNY_ACCOUNT_FACTORY_TYPE, TnyAccountFactory))
+#define TNY_ACCOUNT_FACTORY_CLASS(vtable)    (G_TYPE_CHECK_CLASS_CAST ((vtable), TNY_ACCOUNT_FACTORY_TYPE, TnyAccountFactoryClass))
+#define TNY_IS_ACCOUNT_FACTORY(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), TNY_ACCOUNT_FACTORY_TYPE))
+#define TNY_IS_ACCOUNT_FACTORY_CLASS(vtable) (G_TYPE_CHECK_CLASS_TYPE ((vtable), TNY_ACCOUNT_FACTORY_TYPE))
+#define TNY_ACCOUNT_FACTORY_GET_CLASS(inst)  (G_TYPE_INSTANCE_GET_CLASS ((inst), TNY_ACCOUNT_FACTORY_TYPE, TnyAccountFactoryClass))
+
+typedef struct _TnyAccountFactory TnyAccountFactory;
+typedef struct _TnyAccountFactoryClass TnyAccountFactoryClass;
+
+struct _TnyAccountFactory
+{
+	GObject parent;
+};
+
+struct _TnyAccountFactoryClass
+{
+	GObjectClass parent;
+};
+
+GType                   tny_account_factory_get_type       (void);
+TnyAccountFactory*      tny_account_factory_new            (void);
+
+
+G_END_DECLS
+
+#endif
