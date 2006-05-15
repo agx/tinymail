2395f6326c9aa61e599fd36ac34011753792e15b
Philip Van Hoof <philip@codeminded.be>
2006-05-15 23:03:54 +0000

    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@322 d4f517bd-a70b-0410-9f68-c17655437c52

--- tinymail/tny-summary-window.c
+++ tinymail/tny-summary-window.c
@@ -385,7 +385,7 @@ on_header_view_tree_row_activated (GtkTreeView *treeview, GtkTreePath *path,
 
 			/* Another solution would be to set the ui back to the
 			   folder of this message, and camel-cancel the folder-
-			   request. */
+			   request. Update: This solution has been implemented. */
 
 			msg = tny_msg_folder_iface_get_message (TNY_MSG_FOLDER_IFACE (folder), header);
 			nheader = tny_msg_iface_get_header (TNY_MSG_IFACE (msg));
