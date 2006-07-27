f9e207e91f9233adf4aca4f9581ec15aeed2d898
Philip Van Hoof <philip@codeminded.be>
2006-07-27 20:21:40 +0000

    Little fix for the python binding
    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@595 d4f517bd-a70b-0410-9f68-c17655437c52

--- autogen.sh
+++ autogen.sh
@@ -25,9 +25,5 @@ patch -fp0 < gtk-doc.make.distcheck.patch
 
 # TODO: Put this in a make target (stupid gtk-doc.make doesn't make this possible afaik)
 
+./gentypes.sh $srcdir
 
-find $srcdir -type f -iname "*.h" -printf "#include <%f>\n" | grep -v "\-priv" | grep -v "\-test" | grep -v "config.h" | grep -v "tny-summary-window.h" | grep -v "tny-summary-view.h" | grep -v "tny-msg-folder-list.h" | grep -v "tny-platform-factory.h" | grep -v "tny-account-store.h" | grep -v "tny-device.h" | grep -v "tny-password-dialog.h" | grep -v camel > docs/devel/reference/libtinymail.types
-
-echo >> docs/devel/reference/libtinymail.types
-
-find $srcdir -type f -iname "*.h" -exec grep get_type {} \; | grep -v _tny_list_iterator | grep -v tny_summary_view | grep -v define | grep -v _test | grep -v tny_summary_window |grep -v tny_msg_folder_list | grep -v "tny-platform-factory" | grep -v "tny-account-store" | grep -v "tny-device" | grep -v "tny-password-dialog" |grep -v camel | cut -d " " -f 2- | cut -d "(" -f 1| sed s/\ //g >> docs/devel/reference/libtinymail.types
---
+++ gentypes.sh
@@ -0,0 +1,9 @@
+
+srcdir=$1
+
+find $srcdir -type f -iname "*.h" -printf "#include <%f>\n" | grep -v "\-priv" | grep -v "\-test" | grep -v "config.h" | grep -v "tny-summary-window.h" | grep -v "tny-summary-view.h" | grep -v "tny-msg-folder-list.h" | grep -v "tny-platform-factory.h" | grep -v "tny-account-store.h" | grep -v "tny-device.h" | grep -v "tny-password-dialog.h" | grep -v camel > docs/devel/reference/libtinymail.types
+
+echo >> docs/devel/reference/libtinymail.types
+
+find $srcdir -type f -iname "*.h" -exec grep get_type {} \; | grep -v _tny_list_iterator | grep -v tny_summary_view | grep -v define | grep -v _test | grep -v tny_summary_window |grep -v tny_msg_folder_list | grep -v "tny-platform-factory" | grep -v "tny-account-store" | grep -v "tny-device" | grep -v "tny-password-dialog" |grep -v camel | cut -d " " -f 2- | cut -d "(" -f 1| sed s/\ //g >> docs/devel/reference/libtinymail.types
+
