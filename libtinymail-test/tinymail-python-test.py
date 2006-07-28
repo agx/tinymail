782c0bc70347fd1423ca4bb52f579bee6ae5bea1
Philip Van Hoof <philip@codeminded.be>
2006-07-28 10:15:14 +0000

    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@611 d4f517bd-a70b-0410-9f68-c17655437c52

--- libtinymail-test/tinymail-python-test.py
+++ libtinymail-test/tinymail-python-test.py
@@ -1,4 +1,27 @@
+import gtk
+import gtk.glade
+import gnome
+import gnome.ui
 import tinymail
-import tinymail-camel
+import ui
+import uigtk
+import platform
 
+props = { gnome.PARAM_APP_DATADIR : "/usr/share" }
+pr = gnome.program_init ("E-Mail", "1.0", properties=props)
+xml = gtk.glade.XML ("tinymail-python-test.glade", domain="email")
 
+widget = xml.get_widget ("window")
+foldertree = xml.get_widget ("folderstree")
+headerstree = xml.get_widget ("headerstree")
+hbox = xml.get_widget ("hbox")
+vbox = xml.get_widget ("vbox")
+
+platfact = platform.tny_platform_factory_get_instance ()
+account_store = platfact.new_account_store ()
+accounts = tny_account_tree_model_new ()
+account_store.get_accounts (accounts, 1)
+
+folderstree.set_model (account_store)
+
+gtk.main()
