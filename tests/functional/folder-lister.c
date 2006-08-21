c6f6d25804da4d67f2dd5a8da9f2236bc87185e7
Philip Van Hoof <philip@codeminded.be>
2006-08-21 09:53:22 +0000

    Added some tests
    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@731 d4f517bd-a70b-0410-9f68-c17655437c52

--- tests/functional/folder-lister.c
+++ tests/functional/folder-lister.c
@@ -16,9 +16,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
  
-
-#include <gtk/gtk.h>
-
 #include <tny-list-iface.h>
 #include <tny-iterator-iface.h>
 #include <tny-list.h>
