719fb7f8d9a2c17ffef174a9859578f9203328e0
Philip Van Hoof <philip@codeminded.be>
2006-08-31 11:54:57 +0000

    Created a few new types
    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@853 d4f517bd-a70b-0410-9f68-c17655437c52

---
+++ libtinymail-camel/tny-camel-pop-folder.c
@@ -0,0 +1,132 @@
+/* libtinymail-camel - The Tiny Mail base library for Camel
+ * Copyright (C) 2006-2007 Philip Van Hoof <pvanhoof@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with self library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/* TODO: Refactor to a TnyPOPStoreAccount, TnyPOPStoreAccount and 
+TnyNNTPStoreAccount. Maybe also add a TnyExchangeStoreAccount? This file can 
+stay as an abstract TnyStoreAccount type. */
+
+#include <config.h>
+#include <glib/gi18n-lib.h>
+#include <glib.h>
+#include <string.h>
+
+
+#include <tny-folder-iface.h>
+#include <tny-folder-store-iface.h>
+#include <tny-camel-folder.h>
+#include <tny-camel-pop-folder.h>
+
+#include <camel/camel.h>
+#include <camel/camel-session.h>
+#include <camel/camel-store.h>
+
+#include <tny-folder-iface.h>
+
+#include "tny-camel-account-priv.h"
+#include "tny-camel-store-account-priv.h"
+#include "tny-camel-folder-priv.h"
+#include "tny-camel-common-priv.h"
+
+#include <tny-camel-shared.h>
+#include <tny-account-store-iface.h>
+
+
+static GObjectClass *parent_class = NULL;
+
+
+/**
+ * tny_camel_pop_folder_new:
+ * 
+ *
+ * Return value: A new POP #TnyFolderIface instance implemented for Camel
+ **/
+TnyFolderIface*
+tny_camel_pop_folder_new (void)
+{
+	TnyCamelPOPFolder *self = g_object_new (TNY_TYPE_CAMEL_POP_FOLDER, NULL);
+
+	return TNY_FOLDER_IFACE (self);
+}
+
+static void
+tny_camel_pop_folder_finalize (GObject *object)
+{
+    
+	(*parent_class->finalize) (object);
+
+	return;
+}
+
+static void 
+tny_camel_pop_folder_class_init (TnyCamelPOPFolderClass *class)
+{
+	GObjectClass *object_class;
+
+	parent_class = g_type_class_peek_parent (class);
+	object_class = (GObjectClass*) class;
+
+	object_class->finalize = tny_camel_pop_folder_finalize;
+
+	return;
+}
+
+
+static void
+tny_camel_pop_folder_instance_init (GTypeInstance *instance, gpointer g_class)
+{
+	return;
+}
+
+GType 
+tny_camel_pop_folder_get_type (void)
+{
+	static GType type = 0;
+
+	if (G_UNLIKELY (!camel_type_init_done))
+	{
+		if (!g_thread_supported ()) 
+			g_thread_init (NULL);
+
+		camel_type_init ();
+		camel_type_init_done = TRUE;
+	}
+
+	if (G_UNLIKELY(type == 0))
+	{
+		static const GTypeInfo info = 
+		{
+		  sizeof (TnyCamelPOPFolderClass),
+		  NULL,   /* base_init */
+		  NULL,   /* base_finalize */
+		  (GClassInitFunc) tny_camel_pop_folder_class_init,   /* class_init */
+		  NULL,   /* class_finalize */
+		  NULL,   /* class_data */
+		  sizeof (TnyCamelPOPFolder),
+		  0,      /* n_preallocs */
+		  tny_camel_pop_folder_instance_init    /* instance_init */
+		};
+	    
+		type = g_type_register_static (TNY_TYPE_CAMEL_FOLDER,
+			"TnyCamelPOPFolder",
+			&info, 0);	    
+	}
+
+	return type;
+}
+
