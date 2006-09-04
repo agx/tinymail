22d1825ece1ebdb3fe1c0c3ad22f33970d0b6ec2
Philip Van Hoof <philip@codeminded.be>
2006-09-04 21:48:25 +0000

    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@881 d4f517bd-a70b-0410-9f68-c17655437c52

--- gentypes.sh
+++ gentypes.sh
@@ -1,10 +1,9 @@
 
 srcdir=$1
 
-
-find $srcdir -type f -iname "*.h" -printf "#include <%f>\n" | grep -v "\-priv" | grep -v "\-test" | grep -v "config.h" | grep -v "tny-summary-window.h" | grep -v "tny-summary-view.h" | grep -v "tny-folder-list.h" | grep -v "tny-platform-factory.h" | grep -v "tny-account-store.h" | grep -v "tny-device.h" | grep -v "tny-password-dialog.h" | grep -v "folder-lister.h" | grep -v "platfact.h" | grep -v "device.h" | grep -v "account-store.h" | grep -v camel > docs/devel/reference/libtinymail.types
+find '.' -type f -iname '*.h' -printf '#include <%f>\n' | grep -v "\-priv" | grep -v "\-test" | grep -v platform_include.h | grep -v camel.h | grep -v platfact.h | grep -v device.h | grep -v account-store.h | grep -v config.h | grep -v tny-summary-view.h | grep -v olpc | grep -v gnome | grep -v maemo | grep -v gpe > docs/devel/reference/libtinymail.types
 
 echo >> docs/devel/reference/libtinymail.types
 
-find $srcdir -type f -iname "*.h" -exec grep get_type {} \; | grep -v _tny_list_iterator | grep -v tny_summary_view | grep -v define | grep -v _test | grep -v tny_summary_window |grep -v tny_folder_list | grep -v "tny-platform-factory" | grep -v "tny-account-store" | grep -v "tny-device" | grep -v "device_get_type" | grep -v "tny-password-dialog" |grep -v camel | cut -d " " -f 2- | cut -d "(" -f 1| sed s/\ //g >> docs/devel/reference/libtinymail.types
+find $srcdir -type f -iname "*.h" -exec grep get_type {} \; | grep -v _tny_list_iterator | grep -v tny_summary_view | grep -v define | grep -v _test | grep -v tny_maemo | grep -v tny_gpe | grep -v tny_gnome | grep -v tny_olpc | grep -v _camel_get_type | cut -d " " -f 2- | cut -d "(" -f 1 | sed s/\ //g >> docs/devel/reference/libtinymail.types
 
