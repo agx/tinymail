58e8eb3142ac2711404fcd7ec3196508cd394a5f
Philip Van Hoof <philip@codeminded.be>
2006-10-12 13:17:14 +0000

    Added a new but unfinished TnyMimePartView type
    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@980 d4f517bd-a70b-0410-9f68-c17655437c52

---
+++ libtinymailui/tny-mime-part-view.c
@@ -0,0 +1,115 @@
+/* libtinymailui - The Tiny Mail UI library
+ * Copyright (C) 2006-2007 Philip Van Hoof <pvanhoof@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with self library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <config.h>
+
+#include <tny-mime-part-view.h>
+
+
+/**
+ * tny_mime_part_view_clear:
+ * @self: A #TnyMimePartView instance
+ *
+ * Clear the view @self (show nothing)
+ *
+ * Implementors: this method should clear @self (display nothing, or display
+ * a picture with flowers and nude people if that is how your E-mail client
+ * indicates that there's no mime_part loaded)
+ * 
+ **/
+void
+tny_mime_part_view_clear (TnyMimePartView *self)
+{
+#ifdef DEBUG
+	if (!TNY_MIME_PART_VIEW_GET_IFACE (self)->clear_func)
+		g_critical ("You must implement tny_mime_part_view_clear\n");
+#endif
+
+	TNY_MIME_PART_VIEW_GET_IFACE (self)->clear_func (self);
+	return;    
+}
+
+
+/**
+ * tny_mime_part_view_set_mime_part:
+ * @self: A #TnyMimePartView instance
+ * @mime_part: A #TnyMimePart instace
+ *
+ * Set mime_part of the view @self
+ * 
+ * Implementors: this method should cause the view @self to show the mime_part
+ * @mime_part to the user. Often this means showing the from, to, subject, date
+ * and cc labels.
+ *
+ * #TnyMimePartView is often used in a composition with the #TnyMsgView
+ * type (the #TnyMsgView implementation contains a #TnyMimePartView).
+ *
+ **/
+void
+tny_mime_part_view_set_mime_part (TnyMimePartView *self, TnyMimePart *mime_part)
+{
+#ifdef DEBUG
+	if (!TNY_MIME_PART_VIEW_GET_IFACE (self)->set_mime_part_func)
+		g_critical ("You must implement tny_mime_part_view_set_mime_part\n");
+#endif
+
+	TNY_MIME_PART_VIEW_GET_IFACE (self)->view_mime_part_func (self, mime_part);
+	return;
+}
+
+static void
+tny_mime_part_view_base_init (gpointer g_class)
+{
+	static gboolean initialized = FALSE;
+
+	if (!initialized) {
+		/* create interface signals here. */
+		initialized = TRUE;
+	}
+}
+
+GType
+tny_mime_part_view_get_type (void)
+{
+	static GType type = 0;
+
+	if (G_UNLIKELY(type == 0))
+	{
+		static const GTypeInfo info = 
+		{
+		  sizeof (TnyMimePartViewIface),
+		  tny_mime_part_view_base_init,   /* base_init */
+		  NULL,   /* base_finalize */
+		  NULL,   /* class_init */
+		  NULL,   /* class_finalize */
+		  NULL,   /* class_data */
+		  0,
+		  0,      /* n_preallocs */
+		  NULL    /* instance_init */
+		};
+		type = g_type_register_static (G_TYPE_INTERFACE, 
+			"TnyMimePartView", &info, 0);
+
+		g_type_interface_add_prerequisite (type, G_TYPE_OBJECT);
+	}
+
+	return type;
+}
+
+
---
+++ libtinymailui/tny-mime-part-view.h
@@ -0,0 +1,50 @@
+#ifndef TNY_MIME_PART_VIEW_H
+#define TNY_MIME_PART_VIEW_H
+
+/* libtinymailui - The Tiny Mail UI library
+ * Copyright (C) 2006-2007 Philip Van Hoof <pvanhoof@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with self library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <glib.h>
+#include <glib-object.h>
+
+#include <tny-mime-part.h>
+
+G_BEGIN_DECLS
+
+#define TNY_TYPE_MIME_PART_VIEW             (tny_mime_part_view_get_type ())
+#define TNY_MIME_PART_VIEW(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), TNY_TYPE_MIME_PART_VIEW, TnyMimePartView))
+#define TNY_IS_MIME_PART_VIEW(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), TNY_TYPE_MIME_PART_VIEW))
+#define TNY_MIME_PART_VIEW_GET_IFACE(inst)  (G_TYPE_INSTANCE_GET_INTERFACE ((inst), TNY_TYPE_MIME_PART_VIEW, TnyMimePartViewIface))
+
+typedef struct _TnyMimePartView TnyMimePartView;
+typedef struct _TnyMimePartViewIface TnyMimePartViewIface;
+
+struct _TnyMimePartViewIface 
+{
+	GTypeInterface parent;
+
+	void (*view_mime_part_func) (TnyMimePartView *self, TnyMimePart *part);
+	void (*clear_func) (TnyMimePartView *self); 
+};
+
+GType tny_mime_part_view_get_type();
+
+G_END_DECLS
+
+#endif
