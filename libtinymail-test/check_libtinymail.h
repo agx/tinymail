4cefd58c299f056a40a707b6aaa622c0b52d5219
Oystein Gisnas <oystein@gisnas.net>
2006-12-17 11:28:15 +0000

    Add FolderStore tests and a few extra tests for Folder
    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@1323 d4f517bd-a70b-0410-9f68-c17655437c52

--- libtinymail-test/check_libtinymail.h
+++ libtinymail-test/check_libtinymail.h
@@ -5,13 +5,13 @@
 #include <glib.h>
 #include <check.h>
 
-Suite *create_tny_list_suite (void);
-Suite *create_tny_folder_suite (void);
-Suite *create_tny_folder_store_query_suite (void);
 Suite *create_tny_account_store_suite (void);
 Suite *create_tny_account_suite (void);
 Suite *create_tny_device_suite (void);
+Suite *create_tny_folder_store_query_suite (void);
+Suite *create_tny_folder_suite (void);
 Suite *create_tny_header_suite (void);
+Suite *create_tny_list_suite (void);
 Suite *create_tny_mime_part_suite (void);
 Suite *create_tny_msg_suite (void);
 Suite *create_tny_stream_suite (void);
