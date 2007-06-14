7200ea57fee1229cdb839e67a7f8167c345a9f97
Philip Van Hoof <philip@codeminded.be>
2007-06-14 12:56:47 +0000

    Date received in POP
    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@2166 d4f517bd-a70b-0410-9f68-c17655437c52

--- libtinymail-camel/camel-lite/camel/camel-folder-summary.c
+++ libtinymail-camel/camel-lite/camel/camel-folder-summary.c
@@ -2055,7 +2055,7 @@ message_info_new_from_header(CamelFolderSummary *s, struct _camel_header_raw *h)
 	else if (received)
 		mi->date_received = camel_header_decode_date(received, NULL);
 	else 
-		mi->date_received = 0;
+		mi->date_received = mi->date_sent;
 
 	msgid = camel_header_msgid_decode(camel_header_raw_find(&h, "message-id", NULL));
 	if (msgid) 
