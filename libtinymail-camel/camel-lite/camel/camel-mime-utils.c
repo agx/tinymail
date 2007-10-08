ed155e7087b20efa749b4a4be4369953f7dc54c6
Philip Van Hoof <philip@codeminded.be>
2007-10-08 09:33:40 +0000

    2007-10-08  Philip Van Hoof  <pvanhoof@gnome.org>
            * Merged camel-mime-utils.c with uptream Camel
    
    
    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@2828 d4f517bd-a70b-0410-9f68-c17655437c52

--- libtinymail-camel/camel-lite/camel/camel-mime-utils.c
+++ libtinymail-camel/camel-lite/camel/camel-mime-utils.c
@@ -792,8 +792,8 @@ header_decode_lwsp(const char **in)
 
 	d2(printf("is ws: '%s'\n", *in));
 
-	while (camel_mime_is_lwsp(*inptr) || (*inptr =='(' && *inptr != '\0')) {
-		while (camel_mime_is_lwsp(*inptr) && inptr != '\0') {
+	while ((camel_mime_is_lwsp(*inptr) || *inptr =='(') && *inptr != '\0') {
+		while (camel_mime_is_lwsp(*inptr) && *inptr != '\0') {
 			d2(printf("(%c)", *inptr));
 			inptr++;
 		}
@@ -2094,7 +2094,7 @@ header_decode_domain(const char **in)
 			domain = g_string_append_c(domain, '[');
 			inptr++;
 			header_decode_lwsp(&inptr);
-			while (camel_mime_is_dtext(*inptr)) {
+			while (*inptr && camel_mime_is_dtext(*inptr)) {
 				domain = g_string_append_c(domain, *inptr);
 				inptr++;
 			}
