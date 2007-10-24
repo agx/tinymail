4fd1d4f71062387e77837c745745ee3b35344145
Philip Van Hoof <philip@codeminded.be>
2007-10-24 09:52:54 +0000

    Bugfix
    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@2879 d4f517bd-a70b-0410-9f68-c17655437c52

--- libtinymail-camel/camel-lite/camel/providers/local/camel-maildir-summary.c
+++ libtinymail-camel/camel-lite/camel/providers/local/camel-maildir-summary.c
@@ -212,11 +212,9 @@ char *camel_maildir_summary_info_to_name(const CamelMaildirMessageInfo *info)
 
 		/* Priority flags */
 		priority_flag = info->info.info.flags & CAMEL_MESSAGE_HIGH_PRIORITY;
-		if (priority_flag & flagbits[i].flagbit)
+		if ((priority_flag & flagbits[i].flagbit) == flagbits[i].flagbit)
 			*p++ = flagbits[i].flag;
-		else
-		/* Standard flags*/
-		if (info->info.info.flags & flagbits[i].flagbit)
+		else if (info->info.info.flags & flagbits[i].flagbit)
 			*p++ = flagbits[i].flag;
 	}
 	*p = 0;
@@ -245,11 +243,9 @@ int camel_maildir_summary_name_to_info(CamelMaildirMessageInfo *info, const char
 			for (i=0;i<sizeof(flagbits)/sizeof(flagbits[0]);i++) {
 				/* Priority flags */
 				priority_flag = info->info.info.flags & CAMEL_MESSAGE_HIGH_PRIORITY;
-				if (flagbits[i].flag == c && (priority_flag & flagbits[i].flagbit) == 0)
+				if (flagbits[i].flag == c && (priority_flag & flagbits[i].flagbit) == flagbits[i].flagbit)
 					set |= flagbits[i].flagbit;
-				else
-				/* Standard flags */
-				if (flagbits[i].flag == c && (info->info.info.flags & flagbits[i].flagbit) == 0)
+				else if (flagbits[i].flag == c && (info->info.info.flags & flagbits[i].flagbit) == 0)
 					set |= flagbits[i].flagbit;
 			}
 		}
