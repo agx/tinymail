a8fd83a88e47c89853b91e04a827948a9e2a3e5d
Sergio Villar Senin <svillar@igalia.com>
2007-11-20 13:58:16 +0000

    * reverted last change because it causes hangs when refreshing
    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@2982 d4f517bd-a70b-0410-9f68-c17655437c52

--- libtinymail-camel/tny-camel-folder.c
+++ libtinymail-camel/tny-camel-folder.c
@@ -351,7 +351,8 @@ folder_changed (CamelFolder *camel_folder, CamelFolderChangeInfo *info, gpointer
 		summary = priv->folder->summary;
 
 		if (!change && info->uid_changed != NULL && info->uid_changed->len > 0) {
-			priv->cached_length = camel_folder_get_message_count (priv->folder);
+			/* Commented because it seems to be the source of weird hangs */
+			/* priv->cached_length = camel_folder_get_message_count (priv->folder); */
 			priv->unread_length = camel_folder_get_unread_message_count (priv->folder);
 			urcnted = TRUE;
 			change = tny_folder_change_new (TNY_FOLDER (self));
@@ -360,7 +361,7 @@ folder_changed (CamelFolder *camel_folder, CamelFolderChangeInfo *info, gpointer
 		if (info->uid_added && info->uid_added->len > 0) 
 		{
 			if (!urcnted) {
-				priv->cached_length = camel_folder_get_message_count (priv->folder);
+				/* priv->cached_length = camel_folder_get_message_count (priv->folder); */
 				priv->unread_length = camel_folder_get_unread_message_count (priv->folder);
 				urcnted = TRUE;
 			}
@@ -394,7 +395,7 @@ folder_changed (CamelFolder *camel_folder, CamelFolderChangeInfo *info, gpointer
 		if (info->uid_removed && info->uid_removed->len > 0) 
 		{
 			if (!urcnted) {
-				priv->cached_length = camel_folder_get_message_count (priv->folder);
+				/* priv->cached_length = camel_folder_get_message_count (priv->folder); */
 				priv->unread_length = camel_folder_get_unread_message_count (priv->folder);
 				urcnted = TRUE;
 			}
