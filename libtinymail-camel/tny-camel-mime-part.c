cd576ba08462fee1fd459de95530b7b78f8e8fb3
Philip Van Hoof <philip@codeminded.be>
2007-12-02 14:06:16 +0000

    2007-12-02  Philip Van Hoof  <pvanhoof@gnome.org>
            * Merge from devel/pvanhoof/bs branch
    
    
    
    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@3056 d4f517bd-a70b-0410-9f68-c17655437c52

--- libtinymail-camel/tny-camel-mime-part.c
+++ libtinymail-camel/tny-camel-mime-part.c
@@ -35,6 +35,7 @@
 #include <camel/camel-data-wrapper.h>
 #include <tny-camel-shared.h>
 #include <tny-list.h>
+#include <tny-simple-list.h>
 #include <tny-camel-msg.h>
 
 static GObjectClass *parent_class = NULL;
@@ -203,6 +204,71 @@ tny_camel_mime_part_add_part (TnyMimePart *self, TnyMimePart *part)
 	return TNY_CAMEL_MIME_PART_GET_CLASS (self)->add_part_func (self, part);
 }
 
+static TnyMimePart * 
+recreate_part (TnyMimePart *orig)
+{
+	TnyMimePart *retval;
+	TnyList *list = tny_simple_list_new ();
+	TnyIterator *iter;
+	TnyStream *in_stream = tny_mime_part_get_stream (orig);
+	TnyList *header_pairs = tny_simple_list_new ();
+
+	if (TNY_IS_MSG (orig)) {
+		TnyHeader *hdr = tny_msg_get_header (TNY_MSG (orig));
+		retval = TNY_MIME_PART (tny_camel_msg_new ());
+		_tny_camel_msg_set_header (TNY_CAMEL_MSG (retval), hdr);
+		g_object_unref (hdr);
+	} else
+		retval = tny_camel_mime_part_new ();
+
+	tny_mime_part_construct_from_stream (retval, in_stream, 
+		tny_mime_part_get_content_type (orig));
+
+	tny_mime_part_get_header_pairs (orig, header_pairs);
+	iter = tny_list_create_iterator (header_pairs);
+	while (!tny_iterator_is_done (iter)) {
+		TnyPair *pair = TNY_PAIR (tny_iterator_get_current (iter));
+		tny_mime_part_set_header_pair (retval, 
+			tny_pair_get_name (pair), 
+			tny_pair_get_value (pair));
+		g_object_unref (pair);
+		tny_iterator_next (iter);
+	}
+	g_object_unref (iter);
+	g_object_unref (header_pairs);
+
+	if (tny_mime_part_is_attachment (orig))
+		tny_mime_part_set_filename (retval, tny_mime_part_get_filename (orig));
+	if (tny_mime_part_get_content_location (orig))
+		tny_mime_part_set_content_location (retval, tny_mime_part_get_content_location (orig));
+	tny_mime_part_set_content_id (retval, tny_mime_part_get_content_id (orig));
+	if (tny_mime_part_get_content_type (orig))
+		tny_mime_part_set_content_type (retval, tny_mime_part_get_content_type (orig));
+	if (tny_mime_part_get_description (orig))
+		tny_mime_part_set_description (retval, tny_mime_part_get_description (orig));
+
+	g_object_unref (in_stream);
+
+	tny_mime_part_get_parts (orig, list);
+	iter = tny_list_create_iterator (list);
+	while (!tny_iterator_is_done (iter)) {
+		TnyMimePart *part = TNY_MIME_PART (tny_iterator_get_current (iter));
+		TnyMimePart *add_part;
+
+		add_part = recreate_part (part);
+
+		tny_mime_part_add_part (retval, add_part);
+		g_object_unref (add_part);
+
+		g_object_unref (part);
+		tny_iterator_next (iter);
+	}
+	g_object_unref (iter);
+	g_object_unref (list);
+
+	return retval;
+}
+
 static gint
 tny_camel_mime_part_add_part_default (TnyMimePart *self, TnyMimePart *part)
 {
@@ -216,13 +282,9 @@ tny_camel_mime_part_add_part_default (TnyMimePart *self, TnyMimePart *part)
 
 	g_assert (TNY_IS_MIME_PART (part));
 
-	if (!TNY_IS_CAMEL_MIME_PART (part)) {
-		TnyStream *in_stream = tny_mime_part_get_stream (part);
-		actual_part = tny_camel_mime_part_new ();
-		tny_mime_part_construct_from_stream (actual_part, in_stream, 
-			tny_mime_part_get_content_type (part));
-		g_object_unref (in_stream);
-	} else 
+	if (!TNY_IS_CAMEL_MIME_PART (part))
+		actual_part = recreate_part (part);
+	else 
 		g_object_ref (actual_part);
 
 	g_mutex_lock (priv->part_lock);
