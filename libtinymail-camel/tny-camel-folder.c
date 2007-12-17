1f4437856c651f92e6d6364964c25f4617f65b53
Sergio Villar Senin <svillar@igalia.com>
2007-12-17 12:50:14 +0000

    * Fix in the order of the parameters
    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@3145 d4f517bd-a70b-0410-9f68-c17655437c52

--- libtinymail-camel/tny-camel-folder.c
+++ libtinymail-camel/tny-camel-folder.c
@@ -4901,7 +4901,7 @@ tny_camel_folder_get_folders_async_cancelled_callback (gpointer thr_user_data)
 static void 
 tny_camel_folder_get_folders_async (TnyFolderStore *self, TnyList *list, TnyGetFoldersCallback callback, TnyFolderStoreQuery *query, TnyStatusCallback status_callback, gpointer user_data)
 {
-	TNY_CAMEL_FOLDER_GET_CLASS (self)->get_folders_async_func (self, list, callback, query, status_callback, user_data);
+	TNY_CAMEL_FOLDER_GET_CLASS (self)->get_folders_async_func (self, list, query, callback, status_callback, user_data);
 }
 
 static void 
