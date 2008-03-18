010babcea1be4e6ee42389f75c7a09daeebbb7e0
Sergio Villar Senin <svillar@igalia.com>
2008-03-18 08:40:14 +0000

    * Reverting revision 3494 which is causing SIGSEV's when there is no room in local storage (needs further investigations)
    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@3496 d4f517bd-a70b-0410-9f68-c17655437c52

--- libtinymail-camel/camel-lite/camel/camel-folder-summary.c
+++ libtinymail-camel/camel-lite/camel/camel-folder-summary.c
@@ -3429,7 +3429,7 @@ camel_message_info_free(void *o)
 
 	g_return_if_fail(mi != NULL);
 
-	if (mi->summary && ((CamelObject *)mi->summary)->ref_count > 0) {
+	if (mi->summary) {
 		CAMEL_SUMMARY_LOCK(mi->summary, ref_lock);
 
 		if (mi->refcount >= 1)
