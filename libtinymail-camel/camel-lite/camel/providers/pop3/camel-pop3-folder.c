ca3e081d5d48f4594c352ca5d83fc7200dfcf935
Jose Dapena Paz <jdapena@igalia.com>
2008-05-15 13:46:47 +0000

    * libtinymail-camel/camel-lite/camel/providers/pop3/camel-pop3-folder.c:         * Fix attachment heuristics.
    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@3669 d4f517bd-a70b-0410-9f68-c17655437c52

--- libtinymail-camel/camel-lite/camel/providers/pop3/camel-pop3-folder.c
+++ libtinymail-camel/camel-lite/camel/providers/pop3/camel-pop3-folder.c
@@ -386,26 +386,24 @@ pop3_refresh_info (CamelFolder *folder, CamelException *ex)
 			if (msg)
 			{
 
-				if (mi) {
-					struct _camel_header_raw *h;
+				struct _camel_header_raw *h;
+				
+				h = ((CamelMimePart *)msg)->headers;
+				if (camel_header_raw_find(&h, "X-MSMail-Priority", NULL) &&
+				    !camel_header_raw_find(&h, "X-MS-Has-Attach", NULL)) {
 					
-					h = ((CamelMimePart *)msg)->headers;
-					if (camel_header_raw_find(&h, "X-MSMail-Priority", NULL) &&
-					    !camel_header_raw_find(&h, "X-MS-Has-Attach", NULL)) {
-						
-						((CamelMessageInfoBase *)mi)->flags &= ~CAMEL_MESSAGE_ATTACHMENTS;
-
-					} else if (!camel_header_raw_find (&h, "X-MS-Has-Attach", NULL)) {
-						
-						mi = (CamelMessageInfoBase*) camel_folder_summary_uid (folder->summary, fi->uid);
-						mi->size = (fi->size);
-						/* TNY TODO: This is a hack! But else we need to parse
-						 * BODYSTRUCTURE (and I'm lazy). It needs fixing though. */
-						if (mi->size > 102400)
-							mi->flags |= CAMEL_MESSAGE_ATTACHMENTS;
-						/* ... it does */
-						camel_message_info_free (mi);
-					}
+					((CamelMessageInfoBase *)mi)->flags &= ~CAMEL_MESSAGE_ATTACHMENTS;
+					
+				} else if (!camel_header_raw_find (&h, "X-MS-Has-Attach", NULL)) {
+					
+					mi = (CamelMessageInfoBase*) camel_folder_summary_uid (folder->summary, fi->uid);
+					mi->size = (fi->size);
+					/* TNY TODO: This is a hack! But else we need to parse
+					 * BODYSTRUCTURE (and I'm lazy). It needs fixing though. */
+					if (mi->size > 102400)
+						mi->flags |= CAMEL_MESSAGE_ATTACHMENTS;
+					/* ... it does */
+					camel_message_info_free (mi);
 				}
 
 
