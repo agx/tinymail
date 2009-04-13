06786b202b8b221b99692766836d51ef2423b96b
Sergio Villar Senin <svillar@igalia.com>
2009-04-13 09:09:44 +0000

    Moved a call to prevent an invalid dereference
    git-svn-id: https://svn.tinymail.org/svn/tinymail/trunk@3886 d4f517bd-a70b-0410-9f68-c17655437c52

--- libtinymail-camel/camel-lite/camel/camel-mime-parser.c
+++ libtinymail-camel/camel-lite/camel/camel-mime-parser.c
@@ -1197,6 +1197,8 @@ folder_scan_header(struct _header_scan_state *s, int *lastone)
 			inend = s->inend-s->atleast+1;
 
 			while (inptr<inend) {
+				start = inptr;
+
 				if (!s->midline) {
 					if (folder_boundary_check(s, inptr, lastone)) {
 						if ((s->outptr>s->outbuf))
@@ -1205,8 +1207,6 @@ folder_scan_header(struct _header_scan_state *s, int *lastone)
 						goto header_done;
 					}
 				}
-
-				start = inptr;
 
 				/* goto next line/sentinal */
 				while ((*inptr++)!='\n')
